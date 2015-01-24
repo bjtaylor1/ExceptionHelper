057e0e73573e986316b5d3265f392aab12cf3a09
unknown <Ben.Taylor@HFX1084.siriusfs.com>
2015-01-24 17:37:31 +0000

    1.0.0.0 initial commit

---
+++ ExceptionHelper/ExceptionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using ColoredConsole;
+
+namespace ExceptionHelper
+{
+    public static class ExceptionExtensions
+    {
+        public static void WriteToConsoleInColor(this Exception ex, bool includeBase = true)
+        {
+            var exs = includeBase ? new[] {ex, ex.GetBaseException()}.Distinct().ToArray() : new[] {ex};
+            foreach (var e in exs)
+            {
+                ColorConsole.WriteLine(string.Format("{0}: {1}", e.GetType(), e.Message).Red());
+                ColorConsole.WriteLine(e.StackTrace.Yellow());
+            }
+        }
+
+        public static string ToStringIncludingStackTrace(this Exception ex, bool includeBase = true)
+        {
+            var exs = includeBase ? new[] { ex, ex.GetBaseException() }.Distinct().ToArray() : new[] { ex };
+            return string.Join("\n", exs.Select(e => string.Format("{0}: {1}\n{2}\n", e.GetType().Name, e.Message, e.StackTrace)));
+        }
+    }
+}
